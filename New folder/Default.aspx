0ddaf7c4337f113be65077534901d394736acd0d
Nagamani <koganti.nagamani@gmail.com>
2014-11-30 07:55:57 +0000

    Sample
    First Push

---
+++ New folder/Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Index.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Practice your 11 plus maths</title>
+    <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.1.min.js">
+</script>
+    <link href="Styles/StyleSheet.css" rel="stylesheet" />
+</head>
+<body>
+   <header>
+       <div class="Logo-wrap">
+       <h1> Bucks 11 + Maths </h1></div>
+       <nav>
+            <a href="Default2.html">Index</a>
+            <a href="Default2.aspx">Practice</a>
+            <a href="Default2.aspx">Take Test</a>
+            <a href="Default2.aspx">Circulum</a>
+            <a href="Default2.aspx">Circulum</a>
+            <a href="Default2.aspx">Circulum</a>
+                     
+        </nav>
+   </header>
+         <div id="new-header">
+        <script>
+            $("#new-header").load("Default2.html");
+        </script>
+    </div>
+     <script src="Scripts/JavaScript.js"></script>
+    <footer>
+        <h6>Hello Copy rights reserved</h6>
+    </footer>
+</body>
+</html>
---
+++ New folder/Scripts/JavaScript.js
@@ -0,0 +1,49 @@
+﻿   var xmlhttp = new XMLHttpRequest();
+var url = "Add.txt";
+
+xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var myArr = JSON.parse(xmlhttp.responseText);
+        myFunction(myArr);
+    }
+}
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+function myFunction(arr) {
+    var currentIndex = 0;
+    document.getElementById('num1').value = arr[currentIndex].Number1;
+    document.getElementById('num2').value = arr[currentIndex].Number2;
+    document.getElementById('Forward').click(function () {
+        currentIndex++;
+        if (currentIndex >= arr.length) { currentIndex = 0; }
+        else {
+            document.getElementById('num3').value = ""
+            document.getElementById('num1').value = arr[currentIndex].Number1;
+            document.getElementById('num2').value = arr[currentIndex].Number2;
+        }
+    });
+    document.getElementById('Backward').click(function () {
+        currentIndex--;
+        if (currentIndex >= arr.length) { currentIndex = 0; }
+        else {
+            document.getElementById('num3').value = ""
+            document.getElementById('num1').value = arr[currentIndex].Number1;
+            document.getElementById('num2').value = arr[currentIndex].Number2;
+        }
+    });
+}
+
+function sum() {
+    n1 = parseInt(document.getElementById('num1').value);
+    n2 = parseInt(document.getElementById('num2').value);
+    n3 = n1 + n2;
+    if (document.getElementById('num3').value == "")
+    {  
+        alert('Please enter your answer');
+            
+    }
+                 
+    else if (document.getElementById('num3').value != n3) { alert('Think again! & try'); }
+}
+    
